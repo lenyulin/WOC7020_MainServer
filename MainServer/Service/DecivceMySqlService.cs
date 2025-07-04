2797698f9ea99d311772a6f813c0085a1f680592
Respawn_Joker <33741778+lenyulin@users.noreply.github.com>
2025-07-04 22:48:14 +0800

    Update DecivceMySqlService.cs

--- MainServer/Service/DecivceMySqlService.cs
+++ MainServer/Service/DecivceMySqlService.cs
@@ -10,7 +10,7 @@ namespace MainServer.Service
     public class DecivceMySqlService
     {
         [NotNull]
-        MySqlConnection connection_device_data=new MySqlConnection("server=43.156.64.6;database=device_data;username=lenyulin;password=Java19970203..;");
+        MySqlConnection connection_device_data=new MySqlConnection("server=xxxxx;database=device_data;username=xxxxxx;password=xxxxx;");
         public DecivceMySqlService()
         {
             var res = Conn();
